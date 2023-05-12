c7e31ff0e9aec387318922801f68f3c3e2702929
Richard Benson <richard@rkben.dev>
2023-05-12 19:12:59 -0400

    grep version from package.json

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,10 @@
 cd ..
 yarn install
 yarn dist
-cp 'dist/Android Messages-v5.4.1-linux-x86_64.AppImage' ~/.local/bin/
+
+version=$(grep -Po '(?<="version": ")[0-9.]+' package.json)
+
+cp "dist/Android Messages-v$version-linux-x86_64.AppImage" ~/.local/bin/
 cp resources/icons/512x512.png ~/.local/share/icons/messages.png
 
 cat > ~/.local/share/applications/messages.desktop <<EOF
@@ -10,7 +13,7 @@ Name=Messages
 StartupWMClass=android-messages-desktop
 Comment=Messages by Google - A simple, helpful text messaging app
 GenericName=Android Messages
-Exec=/home/$USER/.local/bin/Android\ Messages-v5.4.1-linux-x86_64.AppImage
+Exec=/home/$USER/.local/bin/Android\ Messages-v$version-linux-x86_64.AppImage
 Icon=messages
 Type=Application
 Categories=Network;InstantMessaging;
